5781bfc6f4b4f8c6fd0d26bbd519bac29f6f4838
Francisco Quintero <franperc@gmail.com>
2021-06-22 00:05:09 -0400

    Fuentes, scroll, menu

--- main.js
+++ main.js
@@ -1,4 +1,33 @@
-let h3Title = document.querySelector(".h3Title h3:first-child");
+const scrollDown = () => {
+    let icon = document.querySelector('#icon');
+    let main = document.querySelector('.main');
+    icon.addEventListener("click", (e) => {
+        e.preventDefault();
+        window.scroll({
+            top: main.clientHeight,
+            left: 0,
+            behavior: "smooth"
+        });
 
+    })
 
-h3Title.style.color = "#e2dad0"
+}
+
+scrollDown();
+
+const scrollUp = () => {
+    let icon2 = document.querySelector('#icon2');
+    let main = document.querySelector('.main');
+    icon2.addEventListener("click", (e) => {
+        e.preventDefault();
+        window.scroll({
+            top: -main.clientHeight,
+            left: 0,
+            behavior: "smooth"
+        });
+
+    })
+
+}
+
+scrollUp();
